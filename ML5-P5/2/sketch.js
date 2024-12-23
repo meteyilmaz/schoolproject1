6812882fc92be583e3f3043d1cdc056ef6ea7910
Mete Yılmaz <39689662+meteyilmaz@users.noreply.github.com>
2024-12-23 12:17:28 +0300

    Update sketch.js

--- ML5-P5/2/sketch.js
+++ ML5-P5/2/sketch.js
@@ -1,9 +1,12 @@
 let video;
 let faceMesh;
 let faces = [];
+let img;
+let triangles=[];
 
 function preload() {
-  faceMesh = ml5.faceMesh({ flipped: false });
+  faceMesh = ml5.faceMesh({ flipped: true });
+  img = loadImage('./clouds.jpg');
 }
 
 function gotFaces(results) {
@@ -12,38 +15,39 @@ function gotFaces(results) {
 
 function setup() {
   createCanvas(640, 480);
-  video = createCapture(VIDEO, { flipped: false });
+  video = createCapture(VIDEO, { flipped: true });
   video.hide();
   faceMesh.detectStart(video, gotFaces);
+  triangles = faceMesh.getTriangles();
 }
 
 function draw() {
+  // translate(-width / 2, -height / 2);
+  background(0);
   image(video, 0, 0);
 
   if (faces.length > 0) {
     let face = faces[0];
-    let lips = face.lips;
-    console.log(lips);
-    
-    beginShape(); 
-    for (let i = 0; i < lips.keypoints.length; i++) {
-      let keypoint = lips.keypoints[i];
 
-    }
-    endShape();
+    beginShape(TRIANGLES);
+    for (let i = 0; i < triangles.length; i++) {
+      let tri = triangles[i];
+      let [a, b, c] = tri;
+      let pointA = face.keypoints[a]; 
+      let pointB = face.keypoints[b];
+      let pointC = face.keypoints[c];
+      
+      stroke(255, 255, 0);
+      fill(random(255), 0, random(255));
+      vertex(pointA.x, pointA.y);
+      vertex(pointB.x, pointB.y);
+      vertex(pointC.x, pointC.y);
+      }
+      endShape(CLOSE);
 
-    let a = face.keypoints[13];
-    let b = face.keypoints[14];
-    let d = dist(a.x, a.y, b.x, b.y);
-
-    let yellowPoint = face.keypoints[94];
-    stroke(255, 255, 0);
-    fill(255, 255, 0);
-    circle(yellowPoint.x, yellowPoint.y, d);
   }
 }
 
 function mousePressed() {
   console.log(faces);
-  
 }
