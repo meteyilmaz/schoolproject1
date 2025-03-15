3df06e1773314f62c094b26d2f9ba7a87c811495
Mete Yılmaz <39689662+meteyilmaz@users.noreply.github.com>
2025-03-15 18:28:02 +0300

    Yoklama listesi eklendi

--- Muhammed_Mustafa-Projects/Face Recognition with Json/script.js
+++ Muhammed_Mustafa-Projects/Face Recognition with Json/script.js
@@ -1,5 +1,6 @@
 const video = document.getElementById("video");
 const canvas = document.getElementById("canvas");
+let yoklama_listesi=[];
 
 await Promise.all([
     faceapi.nets.ssdMobilenetv1.loadFromUri("../models"),
@@ -22,7 +23,7 @@ async function SetupCamera() {
 }
 
 async function GetLabeledFaceDescriptions() {
-    const imageFolder = "./Images";
+    const imageFolder = "./images";
     const imageFiles = await FetchImageFiles(imageFolder);
     return Promise.all(
       imageFiles.map(async (file) => {
@@ -53,13 +54,32 @@ async function FetchImageFiles(folderPath) {
       .filter((file) => file.match(/\.(png|jpg|jpeg)$/i));
 }
 
+function refreshTable(student){
+    const tbodyRef = document.getElementById('table1').getElementsByTagName('tbody')[0];
+    // Insert a row at the end of table
+    const newRow = tbodyRef.insertRow();
+    
+    // Insert a cell at the end of the row
+    var newCell = newRow.insertCell();
+    var newText = document.createTextNode(student.number);
+    newCell.appendChild(newText);
+
+    newCell = newRow.insertCell();
+    newText = document.createTextNode(student.name+" "+student.surname);
+    newCell.appendChild(newText);
+
+    newCell = newRow.insertCell();
+    newText = document.createTextNode(student.class);
+    newCell.appendChild(newText);
+}
+
 async function FaceMatcher() {
     let studentsValue;
 
-    fetch("students.json")
+    fetch("./students.json")
         .then(response => response.json())
         .then(value => {
-            studentsValue = value;
+            studentsValue = value;            
         })
         .catch(error => console.error("Hata: ", error));
 
@@ -80,22 +100,36 @@ async function FaceMatcher() {
         .withFaceDescriptors();
 
         const resizedDetections = faceapi.resizeResults(detections, displaySize);
-        canvas.getContext("2d").clearRect(0, 0, canvas.width, canvas.height);
+        const ctx = canvas.getContext('2d', { willReadFrequently: true });
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
 
         const results = resizedDetections.map((result) => {
             return faceMatcher.findBestMatch(result.descriptor);
         });
 
-        results.forEach((result, i) => {
-            if (studentsValue[result.label]) {
-                console.log("Name: " + studentsValue[result.label].name + " Surname: " + studentsValue[result.label].surname + " Number: " + studentsValue[result.label].number);
-            }
+        results.forEach((result, i) => { //result.label ile dosyanın ismi geliyor örneğin 16.jpg için "16" geliyor.
+            let student = studentsValue.find(el => el.number === result.label);
 
+            if (student) {
+                
+                if ( !yoklama_listesi.find(el=>el.number===student.number) ) {
+                    
+                    yoklama_listesi.push(student);
+
+                    refreshTable(student);
+              
+                } 
+            }
+            
             const box = resizedDetections[i].detection.box;
             const drawBox = new faceapi.draw.DrawBox(box, {
-                label: result
+                // label: studentsValue[result.label].name+" "+studentsValue[result.label].surname+", "+studentsValue[result.label].class
+                label : student.name+" "+student.surname
             });
             drawBox.draw(canvas);
+
+            // console.log(yoklama_listesi);
+            
         });
     }, 100);
 }
@@ -104,3 +138,16 @@ async function Main() {
     await SetupCamera();
     await FaceMatcher();
 }
+
+let btn_clear = document.getElementById("btn_clear");
+btn_clear.addEventListener("click", ()=>{
+    yoklama_listesi=[];
+
+    const tbodyRef = document.getElementById('table1').getElementsByTagName('tbody')[0];
+    const rowCount = tbodyRef.rows.length; // Get the number of rows
+ 
+        // Loop through rows in reverse and delete each one
+        for (let i = rowCount - 1; i >= 0; i--) {
+            tbodyRef.deleteRow(i);
+        }
+})
