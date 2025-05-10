a40d9dc16efc7540a0146bcd0e77cefb7077236a
Mete Yılmaz <39689662+meteyilmaz@users.noreply.github.com>
2025-05-10 14:25:34 +0300

    update

--- completed projects/Face Analysis(Yuz Analizi)/script.js
+++ completed projects/Face Analysis(Yuz Analizi)/script.js
@@ -5,7 +5,9 @@ const ctx = canvas.getContext("2d");
 await Promise.all([
     faceapi.nets.ssdMobilenetv1.loadFromUri("../models"),
     faceapi.nets.faceLandmark68Net.loadFromUri("../models"),
-    faceapi.nets.faceExpressionNet.loadFromUri("../models")
+    faceapi.nets.faceRecognitionNet.loadFromUri("../models"),
+    faceapi.nets.ageGenderNet.loadFromUri("../models"),
+    faceapi.nets.faceExpressionNet.loadFromUri("../models"),
 ]).then(Main);
 
 async function SetupCamera() {
@@ -21,19 +23,35 @@ async function SetupCamera() {
     }
 }
 
-async function FaceExpression() {
-    let faceData = await faceapi.detectAllFaces(video).withFaceLandmarks().withFaceExpressions();
-    
+async function FaceAnalysis() {
+    let faceData = await faceapi.detectAllFaces(video).withFaceLandmarks().withFaceDescriptors().withAgeAndGender().withFaceExpressions();
+
+    canvas.style.position = "absolute";
+    canvas.style.top = "0";
+    canvas.style.left = "0";
+
     canvas.getContext("2d").clearRect(0, 0, canvas.width, canvas.height);
 
     faceData = faceapi.resizeResults(faceData, video);
     faceapi.draw.drawDetections(canvas, faceData);
     faceapi.draw.drawFaceExpressions(canvas, faceData);
 
-    requestAnimationFrame(FaceExpression);
+
+    // console.log(faceapi.draw);
+    
+    faceData.forEach(face => {
+        const {age, gender, genderProbability} = face;
+
+        const genderText = "%" + genderProbability.toFixed(2) +" ihtimalle, " + gender ;
+        const ageText = Math.round(age) + " yaşında";
+        const textField = new faceapi.draw.DrawTextField([genderText, ageText], face.detection.box.topRight);
+        textField.draw(canvas);
+    });
+
+    requestAnimationFrame(FaceAnalysis);
 }
 
 async function Main() {
     await SetupCamera();
-    await FaceExpression();
+    await FaceAnalysis();
 }
